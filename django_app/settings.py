148118cfa2a65100f35203e4d82a0ce26a1bda90
fjord-73 <kt-73-rsbh@softbank.ne.jp>
2021-06-05 01:23:47 +0900

    codes

--- django_app/settings.py
+++ django_app/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 import dj_database_url
